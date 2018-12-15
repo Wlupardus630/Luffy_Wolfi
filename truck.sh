288ac36f851dfb3adc2e893d29ebbd016078cf23
Will Lupardus <33467189+Wlupardus630@users.noreply.github.com>
2018-12-14 21:47:22 -0600

    Update truck.sh

--- truck.sh
+++ truck.sh
@@ -2,6 +2,6 @@
 read -p "Do you prefer Ford or Chevy? " truck
 
 case $truck is 
-Ford) echo "Your parents would be ashamed." ;;
+Ford) echo "Maybe try drivng a Chevy." ;;
 Chevy) echo "Your parents raised you right." ;;
 esac
